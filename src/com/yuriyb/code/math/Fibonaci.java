21b2e47835dcc0cf3b974ac9e80a16409c1cd7ae
YuriyB <yuriybpost@gmail.com>
2015-04-12 23:08:04 +0200

    correction regarding method name

--- src/com/yuriyb/code/math/Fibonaci.java
+++ src/com/yuriyb/code/math/Fibonaci.java
@@ -2,12 +2,12 @@ package com.yuriyb.code.math;
 
 public class Fibonaci {
 
-		static int fibonacci (int n){
-			return (n<=2 ? 1 : fibonacci(n-1) + fibonacci(n-2));
+		static int calcFibonacci (int n){
+			return (n<=2 ? 1 : calcFibonacci(n-1) + calcFibonacci(n-2));
 		}
 		
-		public static void main(String[] args){
+		static void main(String[] args){
 			for (int n=1; n<=16; n++)
-				System.out.println(fibonacci(n)+", ");
+				System.out.println(calcFibonacci(n)+", ");
 		}
 }
